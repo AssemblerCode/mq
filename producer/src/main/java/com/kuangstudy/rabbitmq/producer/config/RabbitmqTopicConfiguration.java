20cd1be6de242a740404e23427e41d93aecbb4ea
jinwh <18205773228@163.com>
2022-10-21 20:17:01 +0800

    添加ttl过期时间

--- producer/src/main/java/com/kuangstudy/rabbitmq/producer/config/RabbitmqTopicConfiguration.java
+++ producer/src/main/java/com/kuangstudy/rabbitmq/producer/config/RabbitmqTopicConfiguration.java
@@ -7,6 +7,9 @@ import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 
 public class RabbitmqTopicConfiguration {
@@ -16,14 +19,19 @@ public class RabbitmqTopicConfiguration {
         return new TopicExchange("topic_order_exchange", true, false);
     }
 
-    @Bean(name = "getSmsQueue")
     public Queue getSmsQueue() {
-        return new Queue("sms_queue", true);
+        Map<String,Object> param=new HashMap<>();
+        param.put("x-message-ttl",5000);
+        Queue sms_queue = new Queue("email_queue", true,false,false,param);
+        return sms_queue;
     }
 
     @Bean(name = "getEmailQueue")
     public Queue getEmailQueue() {
-        return new Queue("email_queue", true);
+        Map<String,Object> param=new HashMap<>();
+        param.put("x-message-ttl",5000);
+        Queue email_queue  = new Queue("email_queue", true,false,false,param);
+        return email_queue;
     }
 
     @Bean(name = "getEmailBinding")
